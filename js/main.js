9c8aaa5125040d1a25e5f88068b4fd8621a49416
nenanen <nena96@live.nl>
2018-02-12 14:02:03 +0100

    Added flat ground

--- js/main.js
+++ js/main.js
@@ -1,35 +1,61 @@
-var camera, scene, renderer,
-    geometry, material, mesh;
+var threejsWorld = {
+    camera:{},
+    scene:{},
+    renderer:{},
+    groundMesh:{},
+    controls:{},
+    
+}
 
     init();
     animate();
 
+
     function init() {
-        scene = new THREE.Scene();
+        //basic threejs init
+        threejsWorld.scene = new THREE.Scene();
+        threejsWorld.scene.fog = new THREE.Fog(0xe4e0ba,200,3000)
 
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-        camera.position.z = 1000;
+        threejsWorld.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );        
+        threejsWorld.controls = new THREE.OrbitControls(threejsWorld.camera);
+        threejsWorld.camera.position.z = 200;
+        threejsWorld.camera.position.y = 100;
+        threejsWorld.camera.rotation.z=0;
+        threejsWorld.controls.update();
+        
+        //CALL INIT OBJECT METHODS FROM HERE
+        initGround()
 
-        geometry = new THREE.BoxGeometry( 200, 200, 200 );
-        material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+        //more general init
+        threejsWorld.renderer = new THREE.WebGLRenderer({alpha:true});
+        threejsWorld.renderer.setSize( window.innerWidth, window.innerHeight );
 
-        mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
+        document.getElementById('world').appendChild( threejsWorld.renderer.domElement );
+        window.addEventListener('resize', windowResize, false);
+    }
+    
+    function initGround(){
+        var geometry = new THREE.PlaneGeometry(2000,2000,5)
+        var material = new THREE.MeshBasicMaterial( { color: 0x68c3c0 , side: THREE.DoubleSide} );
 
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-
-        document.body.appendChild( renderer.domElement );
+        threejsWorld.groundMesh = new THREE.Mesh( geometry, material );
+        threejsWorld.groundMesh.rotateX(-0.5 * Math.PI);
+        threejsWorld.scene.add( threejsWorld.groundMesh );
     }
 
+    
+    //general functions
     function animate() {
         requestAnimationFrame( animate );
-        render();
+        threejsWorld.controls.update();
+        threejsWorld.renderer.render( threejsWorld.scene, threejsWorld.camera );
     }
-
-    function render() {
-        mesh.rotation.x += 0.01;
-        mesh.rotation.y += 0.02;
-
-        renderer.render( scene, camera );
+    
+    function windowResize() {
+	// update camera
+        var height = window.innerHeight;
+        var width = window.innerWidth;
+        threejsWorld.renderer.setSize(width, height);
+        threejsWorld.camera.aspect = width / height;
+        threejsWorld.camera.updateProjectionMatrix();
     }
