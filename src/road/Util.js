fa0827106c90515cfa5801ebf9f28d184057af16
Yatoom <jmapvhoof@gmail.com>
2018-03-05 00:29:14 +0100

    Added extra mathematical functions to Util.

--- src/road/Util.js
+++ src/road/Util.js
@@ -1,4 +1,5 @@
 import * as math from "mathjs";
+import * as _ from "lodash";
 
 export default class Util {
     static doSegmentsIntersect(r1, r2) {
@@ -11,6 +12,21 @@ export default class Util {
     }
 
     static randomAngle(limit) {
-        return math.randomRange(-limit, +limit);
+        return math.random(-limit, +limit);
+    }
+
+    static vectorLength(vector) {
+        vector = _.map(vector, (n) => Math.pow(n, 2));
+        const sum = _.sum(vector);
+        return Math.sqrt(sum);
+    }
+
+    static angleBetween(v1, v2, x=0, y=2) {
+        const v1x = v1[x];
+        const v1y = v1[y];
+        const v2x = v2[x];
+        const v2y = v2[y];
+        const angleRad = Math.acos((v1x * v2x + v1y * v2y) / (this.vectorLength(v1) * this.vectorLength(v2)));
+        return angleRad * 180 / Math.PI
     }
 }
