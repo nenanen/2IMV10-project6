a83c213cf0145ebffcc6d3180dc6ab4142557f16
Yatoom <jmapvhoof@gmail.com>
2018-04-02 22:23:05 +0200

    Fix for placing lots.

--- src/building/Placer.js
+++ src/building/Placer.js
@@ -1,9 +1,7 @@
 import Algebra from "../road/Algebra";
-import * as math from "mathjs";
 import BuildingController from "./building";
 import * as THREE from "three";
 import Lot from "./Lot";
-import Vertex from "../road/Vertex";
 
 export default class Placer {
 
@@ -28,13 +26,12 @@ export default class Placer {
     placeLots(road) {
         let success = 0;
         let tries = 0;
-        let area = Lot.getRandomSize();
-        let lot = Lot.getRandomLot(road, area);
-        let collision = lot.collidesAny(this.qTree);
 
-        let placed = false;
-        while (tries < 3 && placed == false) {
-            
+        while (tries < 3) {
+            let area = Lot.getRandomSize();
+            let lot = Lot.getRandomLot(road, area);
+            let collision = lot.collidesAny(this.qTree);
+
             if (collision) {
                 tries += 1;
                 // let plane = lot.makePlane(0xff0000);
@@ -46,7 +43,6 @@ export default class Placer {
                 this.group.add(building);
                 let plane = lot.makePlane();
                 this.group.add(plane);
-                placed = true;
             }
         }
     }
@@ -63,22 +59,22 @@ export default class Placer {
         var material = new THREE.LineBasicMaterial({
             color: 0x0000ff
         });
-        
+
         var geometry = new THREE.Geometry();
         geometry.vertices.push(
-            new THREE.Vector3( location.center[0], -10, location.center[1] ),
-            new THREE.Vector3( location.center[0], 40, location.center[1] ),
+            new THREE.Vector3(location.center[0], -10, location.center[1]),
+            new THREE.Vector3(location.center[0], 40, location.center[1]),
         );
-        
-        var line = new THREE.Line( geometry, material );
-        this.threejsWorld.scene.add( line );
+
+        var line = new THREE.Line(geometry, material);
+        this.threejsWorld.scene.add(line);
         //building.rotateY(lot.rotation.radians);
 
         //trying to change rotation point and then rotate around objects buildings its center
         var axis = new THREE.Vector3(0, 1, 0);
         var matrix = new THREE.Matrix4();
-        matrix.makeRotationAxis( axis.normalize(), lot.rotation.radians); 
-        building.rotation.setFromRotationMatrix( matrix );
+        matrix.makeRotationAxis(axis.normalize(), lot.rotation.radians);
+        building.rotation.setFromRotationMatrix(matrix);
 
 
         building.translateY(height / 2);
